d1e7a983029deaf5a7712f88c8c4f2c561839bdf
yash110905 <153516277+yash110905@users.noreply.github.com>
2025-05-15 09:19:21 -0400

    Add files via upload

---
+++ week-2-student/week-2-student/includes/templateOne.php
@@ -0,0 +1,6 @@
+<section class="row">
+  <div class="col-sm-12 col-md-12 col-lg-12 p-5 text-center">
+    <!-- this template will display whether we connected to our database -->
+    <?php  ?>
+  </div>
+</section>
---
+++ week-2-student/week-2-student/includes/templateTwo.php
@@ -0,0 +1,11 @@
+<section class="row">
+  <div class="col-sm-12 col-md-12 col-lg-4 p-5 text-center">
+    <h3>I am a fake call to action</h3>
+  </div>
+  <div class="col-sm-12 col-md-6 col-lg-4 p-5 text-center">
+    <h3>I am a fake call to action</h3>
+  </div>
+  <div class="col-sm-12 col-md-6 col-lg-4 p-5 text-center">
+    <h3>I am a fake call to action</h3>
+  </div>
+</section>
